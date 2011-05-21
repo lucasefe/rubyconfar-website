2dc4f7040e36ac2401253388e3e777d98e523e51
Lucas Florio <lucasefe@gmail.com>
2011-05-21 20:08:50 -0300

    Adding Submit Proposal Feature (no style at all, but tests are good to go).

--- lib/models.rb
+++ lib/models.rb
@@ -8,3 +8,16 @@ class Registration
   property :created_at,   DateTime
 
 end
+
+class Proposal
+  include DataMapper::Resource
+
+  property :id,               Serial
+  property :title,            String
+  property :description,      Text
+  property :speaker_name,     String
+  property :speaker_email,    String, :required => true, :unique => true, :format => :email_address
+  property :speaker_bio,      Text
+  property :created_at,       DateTime
+
+end
