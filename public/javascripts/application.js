56449baab42403d7d00545d09fe513309f3bd80c
Lucas Florio <lucasefe@gmail.com>
2011-05-27 18:13:48 -0300

    Making fist and prompt prittier.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,14 @@
 // Typewriter
+
+function moveFist() {
+  $('#fist').animate({
+    backgroundPosition: "(right 0)"
+  }, 2000)
+};
+
 $(function(){
   $.fn.typewriter = function(options) {
-    var addChar = function(element, text, latency) {
-      setTimeout()
-    }
-
-    var self = this;
-    var defaults = {
-      latency: 20
-    }
+    var defaults = { latency: 20 }
 
     options = jQuery.extend(defaults , options);
     return this.each(function(){
@@ -16,34 +16,25 @@ $(function(){
       var text = container.html().split("");
       container.empty();
 
-      var run = function() {
+      var typeNextChar = function() {
         var c = text.shift();
         if (c != null) { 
           container.html(container.html() + c);
-          setTimeout(run, options.latency) 
+          setTimeout(typeNextChar, options.latency);
+        } else if (options.after != null) {
+          options.after();
         };
       }
-      run();
+      typeNextChar();
     });
   };
 });
 
-
+// Default easing
 jQuery.easing.def = "easeOutElastic";
-function FistMove() {
-  $('#fist').animate({
-    backgroundPosition: "(right 0)"
-  }, 2000)
-};
-
-function DoWrite(){
-  $('#screen #display').typewriter()
-};
-
 
 $(document).ready(function() {
-  setTimeout(FistMove, 1000); 
-  setTimeout(DoWrite, 1500); 
+  $('#screen #display').typewriter({ after: moveFist });
 });
 
 // App
