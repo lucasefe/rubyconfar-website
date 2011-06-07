6bc019fe41752971a135ce9c7400585741770aa9
Lucas Florio <lucasefe@gmail.com>
2011-06-07 09:28:02 -0300

    Adding sending email after new proposal.

--- lib/models.rb
+++ lib/models.rb
@@ -1,4 +1,5 @@
 require 'data_mapper'
+require "pony"
 
 class Registration
   include DataMapper::Resource
@@ -16,8 +17,30 @@ class Proposal
   property :title,            String, :required => true
   property :description,      Text, :required => true
   property :speaker_name,     String, :required => true
-  property :speaker_email,    String, :required => true, :format => :email_address
+  property :speaker_email,    String, :required => true, :format => :email_address, :unique => false
   property :speaker_bio,      Text
   property :created_at,       DateTime
 
+
+  after :save, :notify_organizers
+
+  private
+
+  def notify_organizers
+    Pony.mail :to => RubyConf::Website::CONFIG.email_recipients,
+              :subject => "New Proposal from #{speaker_name} about #{title}",
+              :enable_starttls_auto => true,
+              :via => :smtp,
+              :via_options => {
+                :address              => 'smtp.gmail.com',
+                :port                 => '587',
+                :enable_starttls_auto => true,
+                :user_name            => RubyConf::Website::CONFIG.smtp_settings['user_name'],
+                :password             => RubyConf::Website::CONFIG.smtp_settings['password'],
+                :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+                :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+              },
+              :body => self.attributes.map {|k, v| "#{k}: #{v}\n" }.join("\n")
+
+  end
 end
