891172246ac950c17ed5ec347cf7e92653ac9804
Nico Sztabzyb <nico@szstudios.net>
2011-06-24 13:35:24 -0300

    Adding animation for social icons and fist. JS rules.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,49 +1,22 @@
-// Typewriter
-$(function(){
-  $.fn.typewriter = function(options) {
-    var defaults = { latency: 20 }
-
-    options = jQuery.extend(defaults , options);
-    return this.each(function(){
-      var container = $(this);
-      var text = container.html().split("\n");
-      container.empty();
-
-      var typeNextChar = function() {
-        var c = text.shift();
-        if (c != null) { 
-          container.html(container.html() + c);
-          setTimeout(typeNextChar, options.latency);
-        } else if (options.after != null) {
-          options.after();
-        };
-      }
-      typeNextChar();
-    });
-  };
-});
-
 // Default easing
 jQuery.easing.def = "easeOutExpo";
 
+function animateSocial() {
+  $('#social .github_icon').animate( {
+    top: "0px"   
+  }, 
+  1700, 'easeOutBounce',   
+  function() {
+    $('#social .twitter_icon').animate({top: "42px" }, 1700, 'easeOutBounce');
+  });
+  
+}
+
 $(document).ready(function() {
-  //$('#screen #display').typewriter();
   $('#fist').animate({
     backgroundPosition: "(right 0)"
-  }, 3000)
+  }, 3500);
+  
+  setTimeout(animateSocial, 500);
 });
 
-// App
-
-
-$(function()  {
-  
-  $('#social .github_icon').animate({
-  	top: "0px"
-  }, 2000);
-  
-  $('#social .twitter_icon').animate({
-  	top: "42px"
-  }, 3000);
-  
-});
